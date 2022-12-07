e2bd70d8d7c2fb82ef457a56a3979a6171fde2ef
vijayanbu <vijayakumar.anbu1@gmail.com>
2022-12-07 19:01:22 +0530

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MarketingApp from "../components/MarketingApp";
+
+
+const App = () => {
+    return <div>
+        <h1>
+            Hello From Container !!
+        </h1>
+        <hr/>
+        <MarketingApp/>
+    </div>
+}
+
+export default App;
