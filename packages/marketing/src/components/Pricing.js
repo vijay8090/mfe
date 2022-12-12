48cb052076a7456324b46e645884860685fd1a17
vijay anbu <vijayakumar.anbu1@gmail.com>
2022-12-12 19:53:38 +0530

    ma text change

--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -153,7 +153,7 @@ export default function Pricing() {
           color="textPrimary"
           gutterBottom
         >
-          Sample Pricing
+          Pricing
         </Typography>
         <Typography
           variant="h5"
