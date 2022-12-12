38aa63d5204ced664edfeb2fcf39b79f9a154a49
vijay anbu <vijayakumar.anbu1@gmail.com>
2022-12-12 19:40:11 +0530

    text change

--- packages/container/components/Header.js
+++ packages/container/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            MFE App 1
+            MFE App
           </Typography>
           <Button
             color="primary"
